a33da461eaeb9ff72a0b395241e09d5cb5ece6d8
eriche2016 <eriche.hust@gmail.com>
2017-01-05 20:33:40 +0800

    add label transform

---
+++ checkAndOrByteTensor.lua
@@ -0,0 +1,11 @@
+input = torch.rand(10, 10)
+print(input)
+
+input[torch.lt(input, 0.2) or torch.ge(input, 0.8)] = 1 
+input[torch.ge(input, 0.2) and torch.lt(input, 0.6)] = 2
+input[torch.ge(input, 0.6) and torch.lt(input, 0.8)] = 3
+print('this will give wrong answer, so there is not and or or for torch.Tensor')
+-- 1 or 0  is 1 
+-- 0 or 1 is 0
+-- 0 and 1 is 1  
+print(input)
--- data/dataloader.lua
+++ data/dataloader.lua
@@ -89,15 +89,32 @@ function DataLoader:run()
                   -- target = torch.eq(target, 1):double()  
                   -- target map will be value of 255(may ranging from 1 to 255) for displaying
                   -- so here we transform it to label: 1 or 2 
-         
-                    
                   -- local save = true 
                   -- if save then
                   -- image.save('../image.jpg', sample.input)
                   -- image.save('../target.png', target*255) 
                   -- end
-                  -- To do: compute labels to guide different stages 
+                  -- To do: compute merged labels to guide different stages 
+                  -- receptive field: 5 14 40 92 196 -- H x W 
+                  -- s < 1: 1, means background 
+                  -- s >= 150: 1, means background 
+                  local mask_lt1_gt150 = torch.add(torch.lt(target, 1.0), torch.ge(target, 150.0)) 
+                  mask_lt1_gt150[mask_lt1_gt150:ge(1)] = 1 -- remove double counting 
+                  -- 1=< < 10: 2
+                  local mask_ge1_lt10 = torch.cmul(torch.ge(target, 1.0), torch.lt(target, 10.0)) 
+                  -- 10=< < 26: 3
+                  local mask_ge10_lt26 = torch.cmul(torch.ge(target, 10.0), torch.lt(target, 26.0)) 
+                  -- 26=< < 60: 4 
+                  local mask_ge26_lt60 = torch.cmul(torch.ge(target, 26.0), torch.lt(target, 60.0)) 
+                  -- 60=< < 150: 5 
+                  local mask_ge60_lt50 = torch.cmul(torch.ge(target, 60.0), torch.lt(target, 150.0)) 
                   
+
+                  target[mask_lt1_gt150] = 1 -- background 
+                  target[mask_ge1_lt10] = 2 
+                  target[mask_ge10_lt26] = 3
+                  target[mask_ge26_lt60] = 4
+                  target[mask_ge60_lt50] = 5
 
                   if not batch then
                      imageSize = input:size():totable()
